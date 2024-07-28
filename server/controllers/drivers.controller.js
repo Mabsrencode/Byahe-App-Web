ba16f81201983555d0750f6940d59641c6c6589e
Reniel George Mababa <mabsrencode@gmail.com>
2024-07-29 03:02:10 +0800

    appwrite init

--- server/controllers/drivers.controller.js
+++ server/controllers/drivers.controller.js
@@ -4,3 +4,7 @@ const client = new Client();
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
   .setProject("6673fdbd0012f4a44e1c");
+
+let promise = Databases.listDocuments("<DATABASE_ID>", "<COLLECTION_ID>", [
+  Query.equal("title", "Avatar"),
+]);
